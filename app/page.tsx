ba71d3b4138083c566c2fb5d9fab9912e01676d2
PJ-Meaw <pathinya19@gmail.com>
2024-04-17 14:25:24 +0700

    up call whoAmI

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
+'use client'
+import axios from "axios";
+
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(()=>{
+
+    axios.get("http://localhost:4000/auth/whoAmI", {
+      withCredentials : true
+    }).then((res : any)=>{
+      console.log(res)
+    })
+    
+  },[])
   return (
     <main className="w-full h-screen flex justify-center items-center">
       <button className="border border-black p-4 rounded-md">
